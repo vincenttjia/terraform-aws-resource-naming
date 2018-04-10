018377ea39e97853e3c5e19bbb8aa51496ada3e0
Rafi Kurnia Putra <rafi.putra@traveloka.com>
2018-04-10 17:45:05 +0700

    restructure code

--- examples/autoscaling-policy-example/main.tf
+++ examples/autoscaling-policy-example/main.tf
@@ -1,6 +1,11 @@
-module "this" {
-  source = "../../modules/autoscaling-policy/"
-
+locals {
   service_name = "txtdata"
   cluster_role = "app"
+}
+
+module "this" {
+  source = "../../"
+
+  name_prefix   = "${local.service_name}-${local.cluster_role}"
+  resource_type = "autoscaling_policy"
 }
